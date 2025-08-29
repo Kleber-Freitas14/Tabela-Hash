c7748821aae72fe3ed9fe64c285c07b0606e9bee
kleber <kleber.lo.pietro@gmail.com>
2025-08-29 14:07:28 -0300

    Segundo

--- src/Hash/Hash_Tabela.java
+++ src/Hash/Hash_Tabela.java
@@ -1,21 +1,31 @@
 package Hash;
 
 public class Hash_Tabela {
-    private Pessoa[] Tabela;
+    private Pessoa[] tabela;
     private int tamanho;
 
     public Hash_Tabela(int tamanho){
         this.tamanho = tamanho;
-        Tabela = new Pessoa[tamanho];
+        tabela = new Pessoa[tamanho];
     }
+    //
     private int hash (int cpf){
         return Math.abs(cpf % tamanho);
     }
 
-    
+    public void inserir(Pessoa p){
+        int pos = hash(p.getCpf());
 
 
-
+        while (tabela[pos] != null){
+            if (tabela[pos].getCpf() == (p.getCpf())){
+                System.out.println("Erro: CPF já cadastrado!");
+                return;
+            }
+            pos = (pos + 1) % tamanho;
+        }
+        tabela[pos] = p;
+    }
 
 
 }
