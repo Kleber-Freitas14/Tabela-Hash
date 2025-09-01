674794127bd2b8658e058b35f2c79ca44cb4d8f3
kleber <kleber.lo.pietro@gmail.com>
2025-09-01 17:39:29 -0300

    Quarto

---
+++ src/Hash/Main.java
@@ -0,0 +1,62 @@
+package Hash;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Hash_Tabela tabela = new Hash_Tabela(5);
+
+        while (true){
+            System.out.println("---------MENU---------");
+            System.out.println("1 - Cadastrar Pessoa:");
+            System.out.println("2 - Buscar pessoa pelo CPF:");
+            System.out.println("3 - Mostrar todos casdastros:");
+            System.out.println("4 - Excluir cadastro:");
+            System.out.println("5 - Sair:");
+            System.out.println("Opção: ");
+
+            int op = sc.nextInt();
+            sc.nextLine();
+
+            if (op < 1 || op > 5){
+                System.out.println("Opção inválida:");
+                continue;
+            }
+
+            else if (op == 1){
+                System.out.println("Informe o nome:");
+                String nome = sc.nextLine();
+                System.out.println("Informe o número do CPF:");
+                String cpf = sc.nextLine();
+                System.out.println("Informe a idade:");
+                int idade = sc.nextInt();
+
+                Pessoa p = new Pessoa(nome, cpf, idade);
+                tabela.inserir(p);
+                System.out.println("Cadastro realizado!");
+
+            }else if (op == 2){
+                System.out.println("Qual número de CPF deseja buscar?");
+                String cpfBusca = sc.nextLine();
+                Pessoa p = tabela.buscar(cpfBusca);
+                if (p != null){
+                    System.out.println("Cadastro encontrado: " + p);
+                }
+
+            } else if (op == 3){
+                tabela.mostrar();
+
+            } else if (op == 4) {
+                System.out.println("Informe o número do CPF para exclusão.");
+                String cpfExc = sc.nextLine();
+                tabela.excluir(cpfExc);
+
+            }else if (op == 5) {
+                System.out.println("Encerrado.");
+                break;
+            }
+        }
+    sc.close();
+    }
+}
--- src/Hash/Pessoa.java
+++ src/Hash/Pessoa.java
@@ -1,50 +1,40 @@
 package Hash;
 
 public class Pessoa {
-
-
-    private String name;
+    private String nome;
     private String cpf;
     private int idade;
 
-    public Pessoa() {
-        this.name = name;
+    public Pessoa(){
+
+    }
+    public Pessoa(String nome, String cpf, int idade){
+        this.nome = nome;
         this.cpf = cpf;
         this.idade = idade;
-    }
-
-    public Pessoa(String name, String cpf, int idade) {
-        this.name = name;
-        this.cpf = cpf;
-        this.idade = idade;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getCpf() {
         return cpf;
     }
-
-    public void setCpf(int cpf) {
-        this.cpf = java.lang.String.valueOf(cpf);
+    public String getNome(){
+        return nome;
     }
-
-    public int getIdade() {
+    public int getIdade(){
         return idade;
     }
-
-    public void setIdade(int idade) {
+    public void setNome (String nome){
+        this.nome = nome;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    public void setIdade(int idade){
         this.idade = idade;
     }
 
     @Override
     public String toString(){
-      return "Nome: " +  name + "  CPF: " +  cpf + "  Idade: " +  idade;
+        return "Nome: " + nome + " - cpf: " + cpf + " - Idade: " + idade;
     }
 }
